4152c44a9e45e0655663f9ba5474d3a169e97bf2
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2020-10-25 14:14:07 +0200

    sunday task how to be a doctor

---
+++ week-2/sunday-tasks/first-task/main.js
@@ -0,0 +1,13 @@
+const genes = ["ACADM", "GLMN", "RLF", "AZIN2", "CRYZ"]
+
+const strand = []
+strand.push(...genes)
+console.log(strand)
+strand.splice(2,1,genes[genes.length-1])
+strand[strand.length-1] = genes[2]
+console.log(strand)
+let removedItem = strand.splice(3,1)
+strand.push(removedItem[0],removedItem[0])
+console.log(strand)
+strand.unshift("FXT")
+console.log(strand)
